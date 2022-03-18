4b8319cc82bec2cf966e8f4ad198fc220724be36
felicityghb <97568662+felicityghb@users.noreply.github.com>
2022-03-18 12:12:30 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -4,9 +4,10 @@ library(data.table)
 
 diamonds
 
-diamonds%>%
+dd <- diamonds%>%
   select(carat,cut,price)%>%
   filter(cut == "Premium")%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+dd
